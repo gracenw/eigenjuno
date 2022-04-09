dd2fdae5dd35153925dae888f0b88835cc15664d
gracenw <gracen.wallace@gmail.com>
2022-04-09 15:11:28 -0400

    reworking some detection stuff

--- utils.py
+++ utils.py
@@ -3,10 +3,25 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 
-VEC_SIZE = 256 * 256 * 3
 
+VEC_SIZE = 256 * 256 * 3
 svm = pickle.load(open('MODELS/svm_model_opt.sav', 'rb'))
 pca = pickle.load(open('MODELS/pca_model_opt.sav', 'rb'))
+
+
+def contrast_resize(img_loc, new_size):
+    """Returns a CLAHE contrasted image of original found at input location, resized to new_size parameter."""
+    img = cv2.imread(img_loc, 3)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(img)
+    clahe = cv2.createCLAHE(clipLimit = 3.0, tileGridSize = (8, 8))
+    img_l = clahe.apply(l)
+    img_l = cv2.merge((img_l, a, b))
+    final = cv2.cvtColor(img_l, cv2.COLOR_LAB2BGR)
+    final = cv2.cvtColor(final, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(final, new_size, interpolation = cv2.INTER_NEAREST)
+    return img
+
 
 def break_blocks(img):
     """Function to recursively shift, divide, and classify a 2048x2048 image. 
@@ -121,21 +136,53 @@ def break_blocks(img):
             
             detections += unshift_dets
         
-        print('Returning block size', str(block_size))
         return detections
     else:
         return np.zeros((full_size, full_size))
+
+
+def classify(img_loc):
+    """Performs contrasting and classification of new test image found at input location.
+    Returns the detections and results figure."""
+    img = contrast_resize(img_loc, (2048, 2048))
+    img = img[:, :, :3]
     
+    detections = break_blocks(img)
     
-def contrast_resize(img_loc, new_size):
-    """Returns a CLAHE contrasted image of original found at input location, resized to new_size parameter """
-    img = cv2.imread(img_loc, 3)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(img)
-    clahe = cv2.createCLAHE(clipLimit = 3.0, tileGridSize = (8, 8))
-    img_l = clahe.apply(l)
-    img_l = cv2.merge((img_l, a, b))
-    final = cv2.cvtColor(img_l, cv2.COLOR_LAB2BGR)
-    final = cv2.cvtColor(final, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(final, new_size, interpolation = cv2.INTER_NEAREST)
-    return img
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (18, 9));
+
+    ax1.imshow(img);
+    ax1.title.set_text('Original Test Image');
+    ax1.axis('off');
+
+    superimposed = np.copy(img)
+    for i in range(3):
+        superimposed[:, :, i] = detections * img[:, :, i]
+    ax2.imshow(superimposed);
+    ax2.title.set_text('Classifier Decisions\nSuperimposed on Image');
+    ax2.axis('off');
+
+#     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize = (18, 18));
+
+#     ax1.imshow(img);
+#     ax1.title.set_text('Original Test Image');
+#     ax1.axis('off');
+    
+#     normalize = detections / (np.square(detections)).sum()
+#     ax2.imshow(normalize);
+#     ax2.title.set_text('Normalized Detections Across\nResolutions and Shifts');
+#     ax2.axis('off');
+    
+#     threshold = np.where(normalize == 0.5, 1, 0)
+#     ax3.imshow(threshold);
+#     ax3.title.set_text('Decision Threshold == 0.5\nAcross Resolutions and Shifts');
+#     ax3.axis('off');
+
+#     superimposed = np.copy(img)
+#     for i in range(3):
+#         superimposed[:, :, i] = threshold * img[:, :, i]
+#     ax4.imshow(superimposed);
+#     ax4.title.set_text('Decision Threshold\nSuperimposed on Image');
+#     ax4.axis('off');
+    
+    return detections, fig
