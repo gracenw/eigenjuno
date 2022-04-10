aca2fbe698cc5af795d1ee9f84a98df2a24fec2e
gracenw <gracen.wallace@gmail.com>
2022-04-10 16:01:01 -0400

    for archival purposes

--- STITCHING/stitch.py
+++ STITCHING/stitch.py
@@ -6,6 +6,7 @@ import spiceypy as spice
 from scipy.interpolate import RectBivariateSpline
 import csv, json, cv2, os, fnmatch
 from PIL import Image
+from scipy.spatial.transform import Rotation as R
 
 print(spice.tkvrsn("TOOLKIT"))
 
@@ -59,135 +60,142 @@ k2 = [2.7381910042256151e-14, 2.7381910042256151e-14, 2.7381910042256151e-14]
 fl = [10.95637/0.0074, 10.95637/0.0074, 10.95637/0.0074]
 
 def get_junocam_jupiter_rel_pos_orient(time_str, add_seconds=0):
-	"""
-	:param time_str: str (time stamp from image specific json data file)
-	:param add_seconds: int or float (seconds to add to time specified by time str for conveniency)
-	:return: ndarray(3), position
-		ndarray(3,3), orientation matrix
-	"""
-	et = spice.str2et(time_str)+add_seconds
-	pos, light_time = spice.spkpos("Juno", [et], 'IAU_JUPITER', 'NONE', 'JUPITER BARYCENTER')
-	pos = np.array(pos[0])
+    """
+    :param time_str: str (time stamp from image specific json data file)
+    :param add_seconds: int or float (seconds to add to time specified by time str for conveniency)
+    :return: ndarray(3), position
+        ndarray(3,3), orientation matrix
+    """
+    et = spice.str2et(time_str)+add_seconds
+    pos, light_time = spice.spkpos("Juno", [et], 'IAU_JUPITER', 'NONE', 'JUPITER BARYCENTER')
+    JUNO_TO_CUBE = np.matrix([[-0.0059163, -0.0142817, -0.9998805], 
+                              [ 0.0023828, -0.9998954,  0.0142678], 
+                              [-0.9999797, -0.0022981,  0.0059497]])
+    CUBE_TO_CAM = (R.from_euler('zyx', [0.69, -0.469,  0.583])).as_matrix()
+    rot_matrix = CUBE_TO_CAM * JUNO_TO_CUBE
+    pos = rot_matrix * pos.reshape(-1, 1)
+    pos = np.array(pos).ravel()
 
-	orient = spice.pxform("IAU_JUPITER", "JUNO_JUNOCAM", et)
-	orient = np.array(orient)  # orient[0, :] is x-vec, orient[1, :] y-vec, orient[2, :] z-vec
-
-	return pos, orient
+    orient = spice.pxform("IAU_JUPITER", "Juno", et)
+    JUP_TO_JUNO = np.matrix(orient)
+    orient = CUBE_TO_CAM * (JUNO_TO_CUBE * JUP_TO_JUNO)
+    
+    return pos, np.array(orient)
 
 def get_sun_jupiter_rel_pos(time_str, add_seconds=0):
-	et = spice.str2et(time_str) + add_seconds
-	pos, light_time = spice.spkpos("SUN", [et], 'IAU_JUPITER', 'NONE', 'JUPITER BARYCENTER')
-	return np.array(pos[0])
+    et = spice.str2et(time_str) + add_seconds
+    pos, light_time = spice.spkpos("SUN", [et], 'IAU_JUPITER', 'NONE', 'JUPITER BARYCENTER')
+    return np.array(pos[0])
 
 def undistort(cam_x, cam_y, color):
-	# undistort function from juno_junocam_v03.ti changed for using numpy arrays
-	cam_x_old = cam_x.copy()
-	cam_y_old = cam_y.copy()
-	for i in range(5):  # fixed number of iterations for simplicity
-		r2 = (cam_x**2+cam_y**2)
-		dr = 1+k1[color]*r2+k2[color]*r2*r2
-		cam_x = cam_x_old/dr
-		cam_y = cam_y_old/dr
-	return cam_x, cam_y
+    # undistort function from juno_junocam_v03.ti changed for using numpy arrays
+    cam_x_old = cam_x.copy()
+    cam_y_old = cam_y.copy()
+    for i in range(5):  # fixed number of iterations for simplicity
+        r2 = (cam_x**2+cam_y**2)
+        dr = 1+k1[color]*r2+k2[color]*r2*r2
+        cam_x = cam_x_old/dr
+        cam_y = cam_y_old/dr
+    return cam_x, cam_y
 
 def get_lightray_vectors(pixel_x, pixel_y, color):
-	"""
-	:param pixel_x: nd-ndarray of x pixel coords
-	:param pixel_y: nd-ndarray of y pixel coords
-	:param color: int (0-2) specifying color 0:blue, 1:green, 2:red
-	:return: (n+1)d-ndarray of float 3d-lightray vectors in camera reference frame (n+1)-dimension are x,y,z components
-	"""
-	cam_x = pixel_x-cx[color]
-	cam_y = pixel_y-cy[color]
-	cam_x, cam_y = undistort(cam_x, cam_y, color)
-	return np.stack([cam_x, cam_y, np.full_like(cam_x, fl[color])], axis=-1)
+    """
+    :param pixel_x: nd-ndarray of x pixel coords
+    :param pixel_y: nd-ndarray of y pixel coords
+    :param color: int (0-2) specifying color 0:blue, 1:green, 2:red
+    :return: (n+1)d-ndarray of float 3d-lightray vectors in camera reference frame (n+1)-dimension are x,y,z components
+    """
+    cam_x = pixel_x-cx[color]
+    cam_y = pixel_y-cy[color]
+    cam_x, cam_y = undistort(cam_x, cam_y, color)
+    return np.stack([cam_x, cam_y, np.full_like(cam_x, fl[color])], axis=-1)
 
 def distort(cam, color):
-	"""
-	:param cam: ndarray(..,2)
-	:param color: int, color 0:blue 1:green 2:red
-	:return: ndarray(..,2), undistortet values
-	"""
-	r2 = np.sum(cam**2, axis=-1)
-	dr = 1 + k1[color] * r2 + k2[color] * r2**2
-	return cam * dr[...,None]
+    """
+    :param cam: ndarray(..,2)
+    :param color: int, color 0:blue 1:green 2:red
+    :return: ndarray(..,2), undistortet values
+    """
+    r2 = np.sum(cam**2, axis=-1)
+    dr = 1 + k1[color] * r2 + k2[color] * r2**2
+    return cam * dr[...,None]
 
 def get_pixel_coord_from_lightray(ray_dirs, color):
-	behind_sensor = (ray_dirs[...,2] <= 0)
-	ray_dirs[behind_sensor, 2] = 1
-	alpha = ray_dirs[...,2] / fl[color]
-	cam = ray_dirs[...,:2] / alpha[...,None]
-	cam = distort(cam, color)
-	cam[...,0] += cx[color]
-	cam[...,1] += cy[color]
-	return np.where(behind_sensor[...,None], -1, cam)
+    behind_sensor = (ray_dirs[...,2] <= 0)
+    ray_dirs[behind_sensor, 2] = 1
+    alpha = ray_dirs[...,2] / fl[color]
+    cam = ray_dirs[...,:2] / alpha[...,None]
+    cam = distort(cam, color)
+    cam[...,0] += cx[color]
+    cam[...,1] += cy[color]
+    return np.where(behind_sensor[...,None], -1, cam)
 
 x, y = np.indices((STRIPE_LENGTH, STRIPE_HEIGHT))
 # list of precomputed 3d-lighray vectors in camera reference frame for 3 colors 0:blue, 1:green, 2:red
 CAMERA_STRIPE_VECTORS = [get_lightray_vectors(x, y, color).transpose(1, 0, 2) for color in range(3)]
 
 def project_onto_jupiter_surf(pos, direct):
-	"""
-	:param pos: ndarray(..,3), origin of the rays
-	:param direct: ndarray(..,3), direction of rays in whatever order
-	:return: ndarray(..,3), position of projected points closest to pos
-		ndarray(..), mask of if the ray hit the sphere
-	"""
-	b, a = JUPITER_POLAR_RADIUS, JUPITER_EQUATORIAL_RADIUS
-	# equations where the line intersects the jupiter surface
-	q1 = b**2*direct[..., 0]**2+b**2*direct[..., 1]**2+a**2*direct[..., 2]**2
-	q2 = 2*pos[..., 0]*direct[..., 0]*b**2+2*pos[..., 1]*direct[..., 1]*b**2+2*pos[..., 2]*direct[..., 2]*a**2
-	q3 = pos[..., 0]**2*b**2+pos[..., 1]**2*b**2+pos[..., 2]**2*a**2-float(a**2*b**2)
+    """
+    :param pos: ndarray(..,3), origin of the rays
+    :param direct: ndarray(..,3), direction of rays in whatever order
+    :return: ndarray(..,3), position of projected points closest to pos
+        ndarray(..), mask of if the ray hit the sphere
+    """
+    b, a = JUPITER_POLAR_RADIUS, JUPITER_EQUATORIAL_RADIUS
+    # equations where the line intersects the jupiter surface
+    q1 = b**2*direct[..., 0]**2+b**2*direct[..., 1]**2+a**2*direct[..., 2]**2
+    q2 = 2*pos[..., 0]*direct[..., 0]*b**2+2*pos[..., 1]*direct[..., 1]*b**2+2*pos[..., 2]*direct[..., 2]*a**2
+    q3 = pos[..., 0]**2*b**2+pos[..., 1]**2*b**2+pos[..., 2]**2*a**2-float(a**2*b**2)
 
-	p, q = q2/q1, q3/q1    
+    p, q = q2/q1, q3/q1    
 
-	tmp = (0.5*p)**2-q
-	mask = tmp >= 0
+    tmp = (0.5*p)**2-q
+    mask = tmp >= 0
 
-	s = -p*0.5-np.sqrt(tmp*mask)
+    s = -p*0.5-np.sqrt(tmp*mask)
 
-	return (pos+s[..., None]*direct)*mask[..., None], mask
+    return (pos+s[..., None]*direct)*mask[..., None], mask
 
 def rotate_around_axis(n, v, alpha):
-	n /= np.linalg.norm(n)
-	return n*n.dot(v) + np.cos(alpha)*np.cross(np.cross(n, v), n) + np.sin(alpha)*np.cross(n, v)
+    n /= np.linalg.norm(n)
+    return n*n.dot(v) + np.cos(alpha)*np.cross(np.cross(n, v), n) + np.sin(alpha)*np.cross(n, v)
 
 def project_tangential_plane(center, x_extent, y_extent, x_res, y_res, orientation):
-	"""
-	:param center: ndarray(3) of float, center of map on jupiter surface
-	:param x_extent: float, extent of the x-axis of the map in km
-	:param y_extent: float, extent of the y-axis of the map in km
-	:param x_res: int, number of pixels on x-axis
-	:param y_res: int, number of pixels on y-axis
-	:param orientation: float, rotating orientation of map around center in radiants
-	:return: ndarray(x_res, y_res, 3) 3d positions for map pixels
-	"""
-	assert np.sqrt(x_extent**2 + y_extent**2) < JUPITER_POLAR_RADIUS
-	a = JUPITER_POLAR_RADIUS / JUPITER_EQUATORIAL_RADIUS
-	if center[0] == 0 and center[1] == 0:
-		north_tang_vector = np.array([1, 0, 0])
-	else:
-		xy_distance = np.sqrt(center[0]**2+center[1]**2)
-		tmp = -center[2] / (a * xy_distance)
-		north_tang_vector = np.array([tmp * center[0], a * center[1], a])
-		north_tang_vector /= np.linalg.norm(north_tang_vector)
+    """
+    :param center: ndarray(3) of float, center of map on jupiter surface
+    :param x_extent: float, extent of the x-axis of the map in km
+    :param y_extent: float, extent of the y-axis of the map in km
+    :param x_res: int, number of pixels on x-axis
+    :param y_res: int, number of pixels on y-axis
+    :param orientation: float, rotating orientation of map around center in radiants
+    :return: ndarray(x_res, y_res, 3) 3d positions for map pixels
+    """
+    assert np.sqrt(x_extent**2 + y_extent**2) < JUPITER_POLAR_RADIUS
+    a = JUPITER_POLAR_RADIUS / JUPITER_EQUATORIAL_RADIUS
+    if center[0] == 0 and center[1] == 0:
+        north_tang_vector = np.array([1, 0, 0])
+    else:
+        xy_distance = np.sqrt(center[0]**2+center[1]**2)
+        tmp = -center[2] / (a * xy_distance)
+        north_tang_vector = np.array([tmp * center[0], a * center[1], a])
+        north_tang_vector /= np.linalg.norm(north_tang_vector)
 
-	east_tang_vector = np.cross(north_tang_vector, center)
-	east_tang_vector /= np.linalg.norm(east_tang_vector)
+    east_tang_vector = np.cross(north_tang_vector, center)
+    east_tang_vector /= np.linalg.norm(east_tang_vector)
 
-	normal_vector = np.cross(east_tang_vector, north_tang_vector)
-	north_tang_vector = rotate_around_axis(normal_vector, north_tang_vector, orientation)
-	east_tang_vector = rotate_around_axis(normal_vector, east_tang_vector, orientation)
+    normal_vector = np.cross(east_tang_vector, north_tang_vector)
+    north_tang_vector = rotate_around_axis(normal_vector, north_tang_vector, orientation)
+    east_tang_vector = rotate_around_axis(normal_vector, east_tang_vector, orientation)
 
-	x_raster, y_raster = np.meshgrid(np.linspace(-x_extent/2, x_extent/2, x_res),
-									 np.linspace(-y_extent/2, y_extent/2, y_res))
+    x_raster, y_raster = np.meshgrid(np.linspace(-x_extent/2, x_extent/2, x_res),
+                                     np.linspace(-y_extent/2, y_extent/2, y_res))
 
-	raster = center[None, None, :] + x_raster[..., None] * north_tang_vector[None, None, :]\
-			 + y_raster[..., None] * east_tang_vector[None, None, :]
+    raster = center[None, None, :] + x_raster[..., None] * north_tang_vector[None, None, :]\
+             + y_raster[..., None] * east_tang_vector[None, None, :]
 
-	points_on_jup, _ = project_onto_jupiter_surf(raster, -normal_vector[None, None, :])
+    points_on_jup, _ = project_onto_jupiter_surf(raster, -normal_vector[None, None, :])
     
-	return points_on_jup
+    return points_on_jup
 
 ## FRAMELETS
 class Framelet:
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import cv2, pickle
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
-
+import matplotlib.pyplot as plt
 
 VEC_SIZE = 256 * 256 * 3
 svm = pickle.load(open('MODELS/svm_model_opt.sav', 'rb'))
@@ -10,7 +10,13 @@ pca = pickle.load(open('MODELS/pca_model_opt.sav', 'rb'))
 
 
 def contrast_resize(img_loc, new_size):
-    """Returns a CLAHE contrasted image of original found at input location, resized to new_size parameter."""
+    """
+    contrast new test image with CLAHE cv2 function
+    :param img_loc: path to 2048x2048 image
+    :param new_size: 2D nd-array containing new x, y dimensions of image
+    :return: img, resized and contrasted cv2 image
+    """
+    
     img = cv2.imread(img_loc, 3)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
     l, a, b = cv2.split(img)
@@ -23,166 +29,3 @@ def contrast_resize(img_loc, new_size):
     return img
 
 
-def break_blocks(img):
-    """Function to recursively shift, divide, and classify a 2048x2048 image. 
-    Returns a 2048x2048 array containing the sum of the class labels predicted for each pixel at each shift and resolution."""
-    full_size = img.shape[0]
-    block_size = int(full_size / 2)
-    if block_size > 128:
-        shift_size = int(block_size / 2)
-        detections = np.empty((full_size, full_size))
-    
-        # rotate slices of image horizontally
-        for i in range(4):
-            shift_img = np.empty(img.shape)
-            shift_img[0:(full_size - (shift_size * i)), :, :] = np.copy(img[(i * shift_size):full_size, :, :])
-            shift_img[(full_size - (shift_size * i)):full_size, :, :] = np.copy(img[0:(i * shift_size), :, :])
-            
-            blocks = np.empty((4, block_size, block_size, 3))
-            blocks[0, :, :, :] = shift_img[0 : block_size, 0 : block_size, :]
-            blocks[1, :, :, :] = shift_img[0 : block_size, block_size : full_size, :]
-            blocks[2, :, :, :] = shift_img[block_size : full_size, 0 : block_size, :]
-            blocks[3, :, :, :] = shift_img[block_size : full_size, block_size : full_size, :]
-
-            valid_indices = []
-            testing_blocks = np.empty((4, VEC_SIZE))
-            for i in range(4):
-                block = cv2.resize(blocks[i, :, :], (256, 256), interpolation = cv2.INTER_NEAREST)
-                green_locs = np.where((block[:, :, 0] == 0) & (block[:, :, 1] != 0) & (block[:, :, 2] != 0))[0]
-                block = np.reshape(block, VEC_SIZE)
-                zero_locs = np.where(block < 0.035)[0]
-                if (zero_locs.size < block.size / 3) and (green_locs.size < block.size / 10):
-                    valid_indices = np.append(valid_indices, i)
-                    testing_blocks[i, :] = block
-
-            current_dets = np.zeros(4)
-
-            if len(valid_indices) != 0:
-                testing_blocks = testing_blocks[np.array(valid_indices).astype(int), :]
-                transform_test = pca.transform(testing_blocks)
-                split_pred = svm.predict(transform_test)
-                index = 0
-                for i in range(4):
-                    if i in valid_indices:
-                        current_dets[i] = split_pred[index]
-                        index += 1
-
-            block_detections = np.zeros((full_size, full_size))
-
-            block_detections[0 : block_size, 0 : block_size] = np.full((block_size, block_size), current_dets[0]) \
-                        + break_blocks(blocks[0, :, :, :])
-            block_detections[0 : block_size, block_size : full_size] = np.full((block_size, block_size), current_dets[1]) \
-                        + break_blocks(blocks[1, :, :, :])
-            block_detections[block_size : full_size, 0 : block_size] = np.full((block_size, block_size), current_dets[2]) \
-                        + break_blocks(blocks[2, :, :, :])
-            block_detections[block_size : full_size, block_size : full_size] = np.full((block_size, block_size), current_dets[3]) \
-                        + break_blocks(blocks[3, :, :, :])
-            
-            unshift_dets = np.empty((full_size, full_size))
-            unshift_dets[0:(i * shift_size), :] = np.copy(block_detections[(full_size - (shift_size * i)):full_size, :])
-            unshift_dets[(i * shift_size):full_size, :] = np.copy(block_detections[0:(full_size - (shift_size * i)), :])
-            
-            detections += unshift_dets
-
-        # rotate slices of image vertically
-        for i in range(4):        
-            shift_img = np.empty(img.shape)
-            shift_img[:, 0:(full_size - (shift_size * i)), :] = np.copy(img[:, (i * shift_size):full_size, :])
-            shift_img[:, (full_size - (shift_size * i)):full_size, :] = np.copy(img[:, 0:(i * shift_size), :])
-            
-            blocks = np.empty((4, block_size, block_size, 3))
-            blocks[0, :, :, :] = shift_img[0 : block_size, 0 : block_size, :]
-            blocks[1, :, :, :] = shift_img[0 : block_size, block_size : full_size, :]
-            blocks[2, :, :, :] = shift_img[block_size : full_size, 0 : block_size, :]
-            blocks[3, :, :, :] = shift_img[block_size : full_size, block_size : full_size, :]
-
-            valid_indices = []
-            testing_blocks = np.empty((4, VEC_SIZE))
-            for i in range(4):
-                block = cv2.resize(blocks[i, :, :], (256, 256), interpolation = cv2.INTER_NEAREST)
-                green_locs = np.where((block[:, :, 0] == 0) & (block[:, :, 1] != 0) & (block[:, :, 2] != 0))[0]
-                block = np.reshape(block, VEC_SIZE)
-                zero_locs = np.where(block < 0.035)[0]
-                if (zero_locs.size < block.size / 3) and (green_locs.size < block.size / 10):
-                    valid_indices = np.append(valid_indices, i)
-                    testing_blocks[i, :] = block
-
-            current_dets = np.zeros(4)
-
-            if len(valid_indices) != 0:
-                testing_blocks = testing_blocks[np.array(valid_indices).astype(int), :]
-                transform_test = pca.transform(testing_blocks)
-                split_pred = svm.predict(transform_test)
-                index = 0
-                for i in range(4):
-                    if i in valid_indices:
-                        current_dets[i] = split_pred[index]
-                        index += 1
-
-            block_detections = np.zeros((full_size, full_size))
-
-            block_detections[0 : block_size, 0 : block_size] = np.full((block_size, block_size), current_dets[0]) \
-                        + break_blocks(blocks[0, :, :, :])
-            block_detections[0 : block_size, block_size : full_size] = np.full((block_size, block_size), current_dets[1]) \
-                        + break_blocks(blocks[1, :, :, :])
-            block_detections[block_size : full_size, 0 : block_size] = np.full((block_size, block_size), current_dets[2]) \
-                        + break_blocks(blocks[2, :, :, :])
-            block_detections[block_size : full_size, block_size : full_size] = np.full((block_size, block_size), current_dets[3]) \
-                        + break_blocks(blocks[3, :, :, :])
-            
-            unshift_dets = np.empty((full_size, full_size))
-            unshift_dets[:, 0:(i * shift_size)] = np.copy(block_detections[:, (full_size - (shift_size * i)):full_size])
-            unshift_dets[:, (i * shift_size):full_size] = np.copy(block_detections[:, 0:(full_size - (shift_size * i))])
-            
-            detections += unshift_dets
-        
-        return detections
-    else:
-        return np.zeros((full_size, full_size))
-
-
-def classify(img_loc):
-    """Performs contrasting and classification of new test image found at input location.
-    Returns the detections and results figure."""
-    img = contrast_resize(img_loc, (2048, 2048))
-    img = img[:, :, :3]
-    
-    detections = break_blocks(img)
-    
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (18, 9));
-
-    ax1.imshow(img);
-    ax1.title.set_text('Original Test Image');
-    ax1.axis('off');
-
-    superimposed = np.copy(img)
-    for i in range(3):
-        superimposed[:, :, i] = detections * img[:, :, i]
-    ax2.imshow(superimposed);
-    ax2.title.set_text('Classifier Decisions\nSuperimposed on Image');
-    ax2.axis('off');
-
-#     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize = (18, 18));
-
-#     ax1.imshow(img);
-#     ax1.title.set_text('Original Test Image');
-#     ax1.axis('off');
-    
-#     normalize = detections / (np.square(detections)).sum()
-#     ax2.imshow(normalize);
-#     ax2.title.set_text('Normalized Detections Across\nResolutions and Shifts');
-#     ax2.axis('off');
-    
-#     threshold = np.where(normalize == 0.5, 1, 0)
-#     ax3.imshow(threshold);
-#     ax3.title.set_text('Decision Threshold == 0.5\nAcross Resolutions and Shifts');
-#     ax3.axis('off');
-
-#     superimposed = np.copy(img)
-#     for i in range(3):
-#         superimposed[:, :, i] = threshold * img[:, :, i]
-#     ax4.imshow(superimposed);
-#     ax4.title.set_text('Decision Threshold\nSuperimposed on Image');
-#     ax4.axis('off');
-    
-    return detections, fig
